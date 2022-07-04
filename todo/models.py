02efb5a6dedc7438a3992e164db2486ca03609c2
eugene <eugene.earth.solar@gmail.com>
2022-07-04 23:04:42 +0900

    matplotlib

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,5 @@
 from django.db.models.deletion import CASCADE
 from django.db import models
-from matplotlib.pyplot import title
 
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
